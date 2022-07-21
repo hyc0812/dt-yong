e2a194fa582ac7d7c9a14f34674076a924ae6638
yongchang he <hyc0812@163.com>
2022-07-21 10:35:37 -0600

    initialize the repo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+
+
+namespace dt_yong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string adtInstanceUrl = "https://instance-yong.api.wcus.digitaltwins.azure.net"; 
+            var credential = new DefaultAzureCredential();
+            var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), credential);
+            Console.WriteLine($"Service client created – ready to go");
+        }
+    }
+}
