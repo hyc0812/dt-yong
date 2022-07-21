eb9d12e72cdd66aa621037cbcb492749f3a5bdef
yongchang he <hyc0812@163.com>
2022-07-21 11:05:16 -0600

    code added to upload models

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using Azure.DigitalTwins.Core;
 using Azure.Identity;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using Azure;
 
 
 namespace dt_yong
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             string adtInstanceUrl = "https://instance-yong.api.wcus.digitaltwins.azure.net"; 
             var credential = new DefaultAzureCredential();
             var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), credential);
             Console.WriteLine($"Service client created – ready to go");
+            Console.WriteLine();
+            Console.WriteLine($"Upload a model");
+            string dtdl = File.ReadAllText("SampleModel.json");
+            var models = new List<string> { dtdl };
+            // Upload the model to the service
+            await client.CreateModelsAsync(models);
         }
     }
 }
